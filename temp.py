8bca65d89b6d1c29a0c5bf3cea5c3f551c185d59
Maximillian May <mccarthurmay@gmail.com>
2024-05-17 17:34:53 +0900

    add temp.py for temporary usage while on vacation
    create first iteration of winrate checking (net profit)
     - store price if safe_tickers states that ticker has a buy signal

--- temp.py
+++ temp.py
@@ -382,58 +382,13 @@ def loadSettings():
     for database, values in settings.items():
         print(f"{database}: {values}")
 
-#FIXFIXFIXFIX
-def makeWinrate():
-    try:
-        db, dbfile = open_file('winrate_storage')
-    except:
-        db = {}
-    close_file(db, 'winrate_storage')
-
-    try:
-        db, dbfile = open_file('winrate')
-    except:
-        db = {}
-    close_file(db, 'winrate')
-
-    winrate()
-
-
-#FIXFIXFIXFIX
-def winrate():
-    db, dbfile = open_file('safe_tickers')
-    db_w, dbfile_w = open_file('winrate_storage')
-    for ticker, ticker_data in db.items():
-        if ticker not in db_w:
-            if ticker_data['Buy'] == True:
-                price = yf.Ticker(ticker).info['currentPrice']
-                db_w[ticker] = {'Price': price} #add date
-                close_file(db_w, 'winrate_storage')
-
-
-def checkwinrate():
-    #check all tickers in winrate if they are "sellable", if sellable send to winrate.pickle
-    #rsi = rsi_calc(ticker, graph = False)
-    #sell_bool = sell(rsi)
-    pass
+    #if ticker in db and ticker.item[Buy] == True:
 
 def main():
-    #try:
-    #    settings()
-    #except:
-    #    makeSettings()
     try:
-        winrate()
+        settings()
     except:
-        makeWinrate()
-
-    #temp load file
-    with open(f'./storage/winrate.pickle', 'rb') as dbfile:
-        data = pickle.load(dbfile)
-        print(data)
-    with open(f'./storage/winrate_storage.pickle', 'rb') as dbfile:
-        data = pickle.load(dbfile)
-        print(data)
+        makeSettings()
 
 
 #actions
@@ -521,6 +476,7 @@ def main():
         if action == "info":
             ticker = input("What ticker you want: ")
             showinfo(ticker)
+            
 
         if action == "update":
             dbname = input("Name of database: ")
