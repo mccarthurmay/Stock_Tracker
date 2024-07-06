7319ca279386d846e584350cc0c9ccf08338295b
Maximillian May <mccarthurmay@gmail.com>
2024-07-06 17:46:22 -0400

    Make into function

--- data/ml.py
+++ data/ml.py
@@ -3,148 +3,150 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Conv1D, MaxPooling1D, Flatten
-from keras.callbacks import EarlyStopping
 
-#Load dataset
+def ml(ticker):
+    from sklearn.preprocessing import MinMaxScaler
+    from keras.models import Sequential
+    from keras.layers import LSTM, Dense, Dropout, Conv1D, MaxPooling1D, Flatten
+    from keras.callbacks import EarlyStopping
 
-#   - Moving averages added to identify trend direction of stock (20 short term, 75 longer term)
-#       - Bullish crossover 
-#           - when short-term moving average crosses above a longer-term moving average
-#       - Bearish crossover
-#           - Short-term moving average crosses below a longer-term moving average
+    #Load dataset
 
-df = yf.Ticker('F').history(period="3y")
-df['S_MA'] = df['Close'].rolling(window=20).mean()
-df['L_MA'] = df['Close'].rolling(window=50).mean()
-df['RSI'] = df['Close'].diff(1).apply(lambda x: max(x, 0)).rolling(window=14).mean() / df['Close'].diff(1).abs().rolling(window=14).mean() * 100
-df.dropna(inplace=True)
-df_train = df[['Close', 'S_MA', 'L_MA', 'RSI']].values
+    #   - Moving averages added to identify trend direction of stock (20 short term, 75 longer term)
+    #       - Bullish crossover 
+    #           - when short-term moving average crosses above a longer-term moving average
+    #       - Bearish crossover
+    #           - Short-term moving average crosses below a longer-term moving average
 
-
-#Scale data
-
-#   - Scaling data ensures all features have same range
-#       - Normalizes data
-#       - Improves convergence (neural networks are sensitive to scale of input data)
-#       - Avoidf dominance (larger ranges may dominate learning process, neglecting shorter ranges)
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-df_scaled = scaler.fit_transform(df_train)
-
-#Create training dataset
-
-X_train = []
-y_train = []
-for i in range(60, len(df_scaled)):
-    X_train.append(df_scaled[i-60:i, :])
-    y_train.append(df_scaled[i, 0])
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-
-#Build LSTM Model
-#   - Layers are added sequentially, initializing linear stack of layers
-regressor = Sequential()
-
-#Convolutional layer
-
-#   - commonly used for image recognition, however can be applied to spacial relationships (time series data)
-#   - filter specifies number of kernals that the layer will learn
-#   - kernal_size indicates size of convolutional matrix, so the layer will convolve each input sequence with 64 different 1D filters, all size 3
-#   - activation = relu is Rectified Linear Unit, a common activation function that introduces non-linearity
-regressor.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
-regressor.add(MaxPooling1D(pool_size=2))
-
-#LSTM layers
-
-# Long Short-Term Memory
-#       - type of recurrent neural network architecture, different from traditional RNNs in that it remembers long-term dependencies
-#   - memory cell (units)
-#       - maintain a cell state over time, stores information for long periodf
-#   - gates
-#       -forget gate = decides what in information to discard from the cell state
-#       -input gate = updates the cell by selectively adding new information
-#       -output gate = controls flow of information from cell state to output based on current input and previous state
-#   
-regressor.add(LSTM(units=50, return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=50))
-regressor.add(Dropout(0.2))
+    df = yf.Ticker(ticker).history(period="2y")
+    df['S_MA'] = df['Close'].ewm(span=20, adjust=False).mean()
+    df['L_MA'] = df['Close'].ewm(span=50, adjust=False).mean()
+    df['RSI'] = df['Close'].diff(1).apply(lambda x: max(x, 0)).rolling(window=14).mean() / df['Close'].diff(1).abs().rolling(window=14).mean() * 100
+    df.dropna(inplace=True)
+    df_train = df[['Close', 'S_MA', 'L_MA', 'RSI']].values
 
 
-#Dense layer
-#   - fully connected layer, performs a linear operation on input data followed by activation function. Connects every neuron in one layer to every neuron in the next layer, making the network.
-#   - Linear Operation
-#       - Performs matrix-vector multiplication on input data 
-#           - y=f(W⋅x+b), where W is weight of matrix shape (m,n), b is a bias vector of shape (m,), and f is an activation function
-#   - Activation Function
-#       - After (W⋅x+b), the output is passed through the activation function, ReLU , Sigmoid, or Tanh (relu in this case)
-regressor.add(Dense(units=1))
+    #Scale data
 
-#Compile Model
-#   - optimizers adjust weights during training to minimize loss
-#   - loss measues hnow well the model performs on training and validation data
-regressor.compile(optimizer='adam', loss='mean_squared_error')
+    #   - Scaling data ensures all features have same range
+    #       - Normalizes data
+    #       - Improves convergence (neural networks are sensitive to scale of input data)
+    #       - Avoidf dominance (larger ranges may dominate learning process, neglecting shorter ranges)
 
-#Early stopping to prevent overfitting
-#   - prevents overfitting of a model to training data
-#       - during training, the performance is evaluated on a separate validation dataset at the end of each epoch
-early_stopping = EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    df_scaled = scaler.fit_transform(df_train)
 
-#Train Model
-#   - epoch = a complete pass through the entire dataset
-#       - machine model learns from training data, adjusting weights to minimize loss
-#       - increasing epochs allows model to see training data more times
-#   - batch size = number of training examples utilized in one interaction
-regressor.fit(X_train, y_train, epochs=200, batch_size=32, callbacks=[early_stopping])
+    #Create training dataset
 
-#Show Prediction of Past Values
-dataset_total = pd.concat((pd.DataFrame(df_train, columns=['Close', 'S_MA', 'L_MA', 'RSI']), df[['Close', 'S_MA', 'L_MA', 'RSI']]), axis=0)
-inputs = dataset_total[len(dataset_total) - len(df) - 60:].values
-inputs = scaler.transform(inputs)
+    X_train = []
+    y_train = []
+    for i in range(60, len(df_scaled)):
+        X_train.append(df_scaled[i-60:i, :])
+        y_train.append(df_scaled[i, 0])
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
 
-X_test = []
-for i in range(60, len(inputs)):
-    X_test.append(inputs[i-60:i, :])
-X_test = np.array(X_test)
+    #Build LSTM Model
+    #   - Layers are added sequentially, initializing linear stack of layers
+    regressor = Sequential()
 
-predicted_df = regressor.predict(X_test)
-predicted_df = scaler.inverse_transform(np.concatenate([predicted_df, np.zeros((predicted_df.shape[0], 3))], axis=1))[:, 0]
+    #Convolutional layer
 
-#Predict future values
-future_days = 30
-future_inputs = inputs[-60:]
-future_predictions = []
+    #   - commonly used for image recognition, however can be applied to spacial relationships (time series data)
+    #   - filter specifies number of kernals that the layer will learn
+    #   - kernal_size indicates size of convolutional matrix, so the layer will convolve each input sequence with 64 different 1D filters, all size 3
+    #   - activation = relu is Rectified Linear Unit, a common activation function that introduces non-linearity
+    regressor.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+    regressor.add(MaxPooling1D(pool_size=2))
 
-for _ in range(future_days):
-    future_X_test = np.reshape(future_inputs, (1, future_inputs.shape[0], future_inputs.shape[1]))
-    future_predicted = regressor.predict(future_X_test)
-    future_predictions.append(future_predicted[0, 0])
-    future_inputs = np.append(future_inputs[1:], np.concatenate([future_predicted, np.zeros((1, 3))], axis=1), axis=0)
+    #LSTM layers
 
-future_predictions = scaler.inverse_transform(np.concatenate([np.array(future_predictions).reshape(-1, 1), np.zeros((future_days, 3))], axis=1))[:, 0]
-
-#Plotting the results
-plt.plot(df.index, df['Close'], color='black', label='Actual Price')
-#plt.plot(df.index[len(df)-len(predicted_df):], predicted_df, color='gray', label='Predicted Price')
-
-#Append future predictions to the dataset for plotting
-future_indices = pd.date_range(start=df.index[-1], periodf=future_days + 1, freq='B')[1:]
-future_df = pd.DataFrame(future_predictions, index=future_indices, columns=['Close'])
-
-#Plot the moving averages
-plt.plot(df.index, df['S_MA'], color='purple', label='Short Term MA')
-plt.plot(df.index, df['L_MA'], color='red', label='Long Term MA')
-
-plt.plot(future_df.index, future_df['Close'], color='blue', label='Future Predictions')
-plt.title('Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend()
-plt.show()
+    # Long Short-Term Memory
+    #       - type of recurrent neural network architecture, different from traditional RNNs in that it remembers long-term dependencies
+    #   - memory cell (units)
+    #       - maintain a cell state over time, stores information for long periodf
+    #   - gates
+    #       -forget gate = decides what in information to discard from the cell state
+    #       -input gate = updates the cell by selectively adding new information
+    #       -output gate = controls flow of information from cell state to output based on current input and previous state
+    #   
+    regressor.add(LSTM(units=100, return_sequences=True))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units=100))
+    regressor.add(Dropout(0.2))
 
 
-f = open("test.txt", "a")
-f.write(str(future_df['Close']))
+    #Dense layer
+    #   - fully connected layer, performs a linear operation on input data followed by activation function. Connects every neuron in one layer to every neuron in the next layer, making the network.
+    #   - Linear Operation
+    #       - Performs matrix-vector multiplication on input data 
+    #           - y=f(W⋅x+b), where W is weight of matrix shape (m,n), b is a bias vector of shape (m,), and f is an activation function
+    #   - Activation Function
+    #       - After (W⋅x+b), the output is passed through the activation function, ReLU , Sigmoid, or Tanh (relu in this case)
+    regressor.add(Dense(units=1))
+
+    #Compile Model
+    #   - optimizers adjust weights during training to minimize loss
+    #   - loss measues hnow well the model performs on training and validation data
+    regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+    #Early stopping to prevent overfitting
+    #   - prevents overfitting of a model to training data
+    #       - during training, the performance is evaluated on a separate validation dataset at the end of each epoch
+    early_stopping = EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
+
+    #Train Model
+    #   - epoch = a complete pass through the entire dataset
+    #       - machine model learns from training data, adjusting weights to minimize loss
+    #       - increasing epochs allows model to see training data more times
+    #   - batch size = number of training examples utilized in one interaction
+    regressor.fit(X_train, y_train, epochs=200, batch_size=32, callbacks=[early_stopping])
+
+    #Show Prediction of Past Values
+    dataset_total = pd.concat((pd.DataFrame(df_train, columns=['Close', 'S_MA', 'L_MA', 'RSI']), df[['Close', 'S_MA', 'L_MA', 'RSI']]), axis=0)
+    inputs = dataset_total[len(dataset_total) - len(df) - 60:].values
+    inputs = scaler.transform(inputs)
+
+    X_test = []
+    for i in range(60, len(inputs)):
+        X_test.append(inputs[i-60:i, :])
+    X_test = np.array(X_test)
+
+    predicted_df = regressor.predict(X_test)
+    predicted_df = scaler.inverse_transform(np.concatenate([predicted_df, np.zeros((predicted_df.shape[0], 3))], axis=1))[:, 0]
+
+    #Predict future values
+    future_days = 10
+    future_inputs = inputs[-60:]
+    future_predictions = []
+
+    for _ in range(future_days):
+        future_X_test = np.reshape(future_inputs, (1, future_inputs.shape[0], future_inputs.shape[1]))
+        future_predicted = regressor.predict(future_X_test)
+        future_predictions.append(future_predicted[0, 0])
+        future_inputs = np.append(future_inputs[1:], np.concatenate([future_predicted, np.zeros((1, 3))], axis=1), axis=0)
+
+    future_predictions = scaler.inverse_transform(np.concatenate([np.array(future_predictions).reshape(-1, 1), np.zeros((future_days, 3))], axis=1))[:, 0]
+
+    #Plotting the results
+    plt.plot(df.index, df['Close'], color='black', label='Actual Price')
+    plt.plot(df.index[len(df)-len(predicted_df):], predicted_df, color='gray', label='Predicted Price')
+
+    #Append future predictions to the dataset for plotting
+    future_indices = pd.date_range(start=df.index[-1], periods=future_days + 1, freq='B')[1:]
+    future_df = pd.DataFrame(future_predictions, index=future_indices, columns=['Close'])
+
+    #Plot the moving averages
+    plt.plot(df.index, df['S_MA'], color='purple', label='Short Term MA')
+    plt.plot(df.index, df['L_MA'], color='red', label='Long Term MA')
+
+    plt.plot(future_df.index, future_df['Close'], color='blue', label='Future Predictions')
+    plt.title('Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+
+
+    f = open("test.txt", "a")
+    f.write(str(future_df['Close']))
